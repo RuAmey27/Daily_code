3e7bf6f758ca948c988f021f693b774b2766c463
RuAmey27 <ameypande2709@gmail.com>
2024-01-22 22:57:14 +0530

    another day another approach for easypronountiatio

---
+++ easypron.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            String s = sc.next(); // Use sc.next() to read the input string directly
+            char ch[] = s.toCharArray();
+
+            int count = 0;
+            boolean is = true;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (ch[j] != 'a' && ch[j] != 'e' && ch[j] != 'i' && ch[j] != 'o' && ch[j] != 'u') {
+                    count++;
+                    if (count >= 4) {
+                        is = false;
+                        break; // No need to continue checking if count is already 4 or more
+                    }
+                } else {
+                    count = 0; // Reset count only when a vowel is encountered
+                }
+            }
+
+            if (is) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
