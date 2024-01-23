e3535b632af9ff1fdda9812996b18ea16eec6535
RuAmey27 <ameypande2709@gmail.com>
2024-01-23 23:25:11 +0530

    water_for_bath problem

---
+++ water_bath.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int t,x,y;
+		Scanner sc=new Scanner(System.in);
+		t=sc.nextInt();
+		while(t!=0)
+		{
+            x=sc.nextInt();
+            y=sc.nextInt();
+            System.out.println(x/(2*y));
+            t--;
+		}
+		
+	}
+}
