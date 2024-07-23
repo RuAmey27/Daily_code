28f5920c358c212ad01fb3a70d5c8f67f11415d7
AMEY ANANTA PANDE <112720582+RuAmey27@users.noreply.github.com>
2024-07-23 14:33:39 +0530

    Time: 6 ms (75.35%), Space: 44.4 MB (41.14%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -1,34 +1,34 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-        
+       // Step 1: Count the frequency of each number
         Map<Integer, Integer> frequencyMap = new HashMap<>();
         for (int num : nums) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer> numList = new ArrayList<>();
-        for (int num : nums) {
-            numList.add(num);
-        }
-        
-        Collections.sort(numList, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                int freqA = frequencyMap.get(a);
-                int freqB = frequencyMap.get(b);
-                if (freqA != freqB) {
-                    return Integer.compare(freqA, freqB);
-                } else {
-                    return Integer.compare(b, a);
-                }
+        // Step 2: Get a list of keys and sort it based on frequency and value
+        List<Integer> keys = new ArrayList<>(frequencyMap.keySet());
+        Collections.sort(keys, (a, b) -> {
+            int freqA = frequencyMap.get(a);
+            int freqB = frequencyMap.get(b);
+            if (freqA != freqB) {
+                return Integer.compare(freqA, freqB);  // Sort by frequency
+            } else {
+                return Integer.compare(b, a);  // If same frequency, sort by value descending
             }
         });
 
-        for (int i = 0; i < numList.size(); i++) {
-            nums[i] = numList.get(i);
+        // Step 3: Construct the result array
+        int[] result = new int[nums.length];
+        int index = 0;
+        for (int key : keys) {
+            int frequency = frequencyMap.get(key);
+            for (int i = 0; i < frequency; i++) {
+                result[index++] = key;
+            }
         }
 
-        return nums;
+        return result;
     }
 
 }
