2941c563140a1ac3e6fb77a7089ef0db8336f419
AMEY ANANTA PANDE <112720582+RuAmey27@users.noreply.github.com>
2024-08-01 15:07:57 +0530

    Time: 0 ms (100%), Space: 42.5 MB (32%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,12 +9,27 @@
  * }
  */
 class Solution {
+    // public ListNode reverseList(ListNode head) {
+    //     if(head==null) return null;
+    //     if(head.next== null) return head;
+    //     ListNode temp= reverseList(head.next);
+    //     head.next.next = head;
+    //     head.next = null;
+    //     return temp ;
+    // }
+
     public ListNode reverseList(ListNode head) {
-        if(head==null) return null;
-        if(head.next== null) return head;
-        ListNode temp= reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return temp ;
+    ListNode prev = null;
+    ListNode current = head;
+    
+    while (current != null) {
+        ListNode next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
     }
+    
+    return prev;
+}
+
 }
