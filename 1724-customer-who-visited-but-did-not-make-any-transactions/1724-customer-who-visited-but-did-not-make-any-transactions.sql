5861ff16541234c5b51d866aab736555b40d4f2d
Amey Pande <112720582+RuAmey27@users.noreply.github.com>
2024-08-20 12:31:20 +0530

    Time: 1052 ms (97.39%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,8 +1,16 @@
-SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
-FROM Visits v
-LEFT JOIN Transactions t
-ON v.visit_id = t.visit_id
-WHERE t.transaction_id IS NULL
-GROUP BY v.customer_id
-ORDER BY v.customer_id;
+-- SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
+-- FROM Visits v
+-- LEFT JOIN Transactions t
+-- ON v.visit_id = t.visit_id
+-- WHERE t.transaction_id IS NULL
+-- GROUP BY v.customer_id
+-- ORDER BY v.customer_id;
 
+SELECT customer_id, COUNT(visit_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (
+    SELECT DISTINCT visit_id 
+    FROM Transactions
+)
+GROUP BY customer_id
+ORDER BY customer_id;
