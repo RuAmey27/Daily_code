7dbf5cea724ca344f71775573915c218b2818e62
Amey Pande <112720582+RuAmey27@users.noreply.github.com>
2024-09-18 10:52:53 +0530

    Time: 6 ms (54.52%), Space: 43.3 MB (48.06%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,19 +1,13 @@
 class Solution {
-   public String largestNumber(int[] nums) {
+ 
     
-       String[] numStrs = new String[nums.length];
+       public String largestNumber(int[] nums) {
+        String[] numStrs = new String[nums.length];
         for (int i = 0; i < nums.length; i++) {
             numStrs[i] = String.valueOf(nums[i]);
         }
 
-        Arrays.sort(numStrs, new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                String order1 = a + b;
-                String order2 = b + a;
-                return order2.compareTo(order1);
-            }
-        });
+        Arrays.sort(numStrs, getComparator());
 
         if (numStrs[0].equals("0")) {
             return "0";
@@ -21,4 +15,15 @@ class Solution {
 
         return String.join("", numStrs);
     }
+
+    private Comparator<String> getComparator() {
+        return new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String order1 = a + b;
+                String order2 = b + a;
+                return order2.compareTo(order1);
+            }
+        };
+}
 }
