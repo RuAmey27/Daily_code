c769ad2d5dc3eb09068413252452d035a2ce8f42
Amey Pande <112720582+RuAmey27@users.noreply.github.com>
2024-09-20 12:23:23 +0530

    Time: 217 ms (29.21%), Space: 45.5 MB (10.19%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,33 +1,14 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
-        String temp = s + "#" + rev;
-
-        int[] lps = computeLongestPrefixSArray(temp);
-
-        return rev.substring(0, s.length() - lps[temp.length() - 1]) + s;
-    }
-
-    private int[] computeLongestPrefixSArray(String s) {
         int n = s.length();
-        int[] lps = new int[n];
-        int len = 0;
-        int i = 1;
+        String rev = new StringBuilder(s).reverse().toString();
 
-        while (i < n) {
-            if (s.charAt(i) == s.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else {
-                if (len != 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
+        for (int i = 0; i < n; i++) {
+            if (s.substring(0, n - i).equals(rev.substring(i))) {
+                return rev.substring(0, i) + s;
             }
         }
-        return lps;
+
+        return "";
     }
 }
