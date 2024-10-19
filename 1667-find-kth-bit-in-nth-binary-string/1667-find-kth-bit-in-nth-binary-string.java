d16c7033c2cba16d8ce92c18be07a94b29fa8b8e
Amey Pande <112720582+RuAmey27@users.noreply.github.com>
2024-10-19 19:53:48 +0530

    Time: 0 ms (100%), Space: 40.9 MB (68.68%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
@@ -1,24 +1,24 @@
 class Solution {
     public char findKthBit(int n, int k) {
-   return findKthBitRecursive(n, k);
+   while (n > 1) {
+            int length = (1 << n) - 1;  // Length of Sn
+            int mid = length / 2 + 1;   // Middle index of Sn
+            
+            if (k == mid) {
+                return '1';  // Middle character is always '1'
+            } else if (k > mid) {
+                k = length - k + 1;  // Mirror the index to the first half
+               char i = invert(findKthBit(n - 1, k));  // Invert the result
+                return i;
+            } else {
+                n--;  // Move to Sn-1 if k is in the first half
+            }
+        }
+        
+        return '0';  // Base case for S1
     }
 
-    private char findKthBitRecursive(int n, int k) {
-        if (n == 1) {
-            return '0';
-        }
-        
-        int length = (1 << n) - 1;
-        int mid = length / 2 + 1;
-        
-        if (k == mid) {
-            return '1';
-        } else if (k < mid) {
-            return findKthBitRecursive(n - 1, k);
-        } else {
-            int mirroredIndex = mid - (k - mid);
-            char mirroredChar = findKthBitRecursive(n - 1, mirroredIndex);
-            return mirroredChar == '0' ? '1' : '0';
-        }
+    private char invert(char bit) {
+        return bit == '0' ? '1' : '0';
     }
 }
